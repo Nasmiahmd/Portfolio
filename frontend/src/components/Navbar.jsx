314b8cc689627a78242df4ea02a7d7cc09f28dc2
Nasmiahmd <ahmednasmi2002@gmail.com>
2026-06-03 21:27:29 +0530

    HomePage Done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { GoProjectSymlink } from "react-icons/go";
+import { GiHumanTarget } from "react-icons/gi";
+import { IoIosContact } from "react-icons/io";
+import { MdOutlineSelfImprovement } from "react-icons/md";
+import { FaRegMessage } from "react-icons/fa6";
+import { BiHappyHeartEyes } from "react-icons/bi";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const menuRef = useRef(null);
+
+    useEffect(()=> {
+        const handleOutsideClick = (event) => {
+            if(open && menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick)
+        }
+    },[open])
   return (
-    <nav className='relative bg-navbar text-navbar-text p-4 rounded-b-2xl'>
-        <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex justify-between text-xl font-sans'>
-            <div className=''>
-                <span className='font-extrabold italic'>Nasmi</span>
+    <nav className='relative shadow-md bg-navbar text-navbar-text py-4 px-8 rounded-b-2xl'>
+        <div className='flex items-center justify-between p-3'>
+            <h1 className='flex gap-1 text-xl font-bold hover:animate-ping transition-all duration-75'>Home</h1>
+            <div className='md:flex gap-4 hidden text-lg font-semibold'>
+                <h1 className='flex items-center gap-1 hover:border-0 ring-1 hover:ring-3 ring-amber-100 hover:shadow-amber-100 hover:shadow-md hover:bg-white hover:text-slate-900 rounded-lg cursor-pointer py-2 px-2 transition-all duration-75'><GoProjectSymlink />Projects</h1>
+                <h1 className='flex items-center gap-1 hover:border-0 ring-1 hover:ring-3 ring-amber-100 hover:shadow-amber-100 hover:shadow-md hover:bg-white hover:text-slate-900 rounded-lg cursor-pointer py-2 px-2 transition-all duration-75'><GiHumanTarget />Skills</h1>
+                <h1 className='flex items-center gap-1 hover:border-0 ring-1 hover:ring-3 ring-amber-100 hover:shadow-amber-100 hover:shadow-md hover:bg-white hover:text-slate-900 rounded-lg cursor-pointer py-2 px-2 transition-all duration-75'><IoIosContact />Contact</h1>
+                <h1 className='flex items-center gap-1 hover:border-0 ring-1 hover:ring-3 ring-amber-100 hover:shadow-amber-100 hover:shadow-md hover:bg-white hover:text-slate-900 rounded-lg cursor-pointer py-2 px-2 transition-all duration-75'><MdOutlineSelfImprovement />About</h1>
+                <h1 className='flex items-center gap-1 hover:border-0 ring-1 hover:ring-3 ring-amber-100 hover:shadow-amber-100 hover:shadow-md hover:bg-white hover:text-slate-900 rounded-lg cursor-pointer py-2 px-2 transition-all duration-75'><FaRegMessage />Leave a message</h1>
             </div>
-            <div className='flex font-semibold gap-4 '>
-                <span>About</span>
-                <span>Projects</span>
-                <span>Skills</span>
-                <span>Leave a message</span>
-            </div>
+            <button className='md:hidden font-bold text-xl' onClick={()=>setOpen(!open)}><BiDotsVerticalRounded className='text-2xl'/></button>
+        </div>
+        <div ref={menuRef}>
+            {open && (
+                <ul className='md:hidden text-xl flex flex-col items-center gap-4 absolute top-14 right-2 m-w-56 bg-white shadow-lg rounded-lg p-4 text-navbar transition-all duration-75'>
+                    <li className='flex items-center gap-2 ring-1 hover:ring-2 hover:border-0 hover:border-navbar hover:shadow-slate-600 hover:shadow-md hover:bg-navbar hover:text-white rounded-lg cursor-pointer py-1 px-2 transition-all duration-75'><GoProjectSymlink /> Projects</li>
+                    <li className='flex items-center gap-2 ring-1 hover:ring-2 hover:border-0 hover:border-navbar hover:shadow-slate-600 hover:shadow-md hover:bg-navbar hover:text-white rounded-lg cursor-pointer py-1 px-2 transition-all duration-75'><GiHumanTarget />Skills</li>
+                    <li className='flex items-center gap-2 ring-1 hover:ring-2 hover:border-0 hover:border-navbar hover:shadow-slate-600 hover:shadow-md hover:bg-navbar hover:text-white rounded-lg cursor-pointer py-1 px-2 transition-all duration-75'><IoIosContact />Contact</li>
+                    <li className='flex items-center gap-2 ring-1 hover:ring-2 hover:border-0 hover:border-navbar hover:shadow-slate-600 hover:shadow-md hover:bg-navbar hover:text-white rounded-lg cursor-pointer py-1 px-2 transition-all duration-75'><MdOutlineSelfImprovement />About</li>
+                    <li className='flex items-center gap-2 ring-1 hover:ring-2 hover:border-0 hover:border-navbar hover:shadow-slate-600 hover:shadow-md hover:bg-navbar hover:text-white rounded-lg cursor-pointer py-1 px-2 transition-all duration-75'><FaRegMessage />Leave a message</li>
+                </ul>
+            )}
         </div>
     </nav>
   )
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 import nasmiPic from "../assets/nasmi-pic.jpg";
+import { TypeAnimation } from "react-type-animation";
+
 const Homepage = () => {
   return (
     <>
-        <Navbar />
-        <div>
-            <h1 className='text-center pt-7 text-4xl font-bold italic'>Hey folks, I'm Nasmi</h1>
-            <hr className='max-w-3xs mx-auto text-heading-hr border-4 mt-0.5 rounded-2xl '/>
-            <img className='mx-auto max-w-xs mt-8 rounded-full border-4 border-gray-600' src={nasmiPic} alt="Nasmi's Picture" />
+      <Navbar />
+      <div>
+        <h1 className="text-center pt-7 text-xl sm:text-2xl md:text-4xl font-bold italic">
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "Hey folks, My self Nasmi.",
+              1500, // wait 1s before replacing "Mice" with "Hamsters"
+              "Here We are 👋.",
+              1500,
+            ]}
+            wrapper="span"
+            speed={40}
+            style={{ fontSize: "1.5em", display: "inline-block" }}
+            repeat={Infinity}
+          />
+        </h1>
+        <hr className="max-w-3xs mx-auto text-heading-hr border-4 mt-0.5 rounded-2xl " />
+        <div className="mt-12 xl:mt-28 flex flex-col-reverse xl:flex-row max-w-full mx-auto items-center xl:justify-between px-6 xl:px-0 gap-8 xl:gap-0">
+          <div className="text-center xl:text-left ps-0 xl:ps-56">
+            <h2 className="text-5xl sm:text-7xl xl:text-9xl hover:text-shadow-sm hover:text-shadow-amber-100 hover:animate-bounce transition-shadow duration-75">Software</h2>
+            <h2 className="text-5xl sm:text-7xl xl:text-9xl hover:text-shadow-sm hover:text-shadow-amber-100 hover:animate-pulse transition-shadow duration-75">Engineer</h2>
+            <p className="text-base sm:text-xl text-slate-600 hover:text-slate-200 max-w-2xl mx-auto py-6 text-wrap">
+              I'm a Software Engineer based in Sri Lanka, I'll Help you through my knowledge.
+            </p>
+          </div>
+          <img
+            className="mx-auto w-64 h-64 sm:w-80 sm:h-80 xl:w-auto xl:h-auto xl:max-w-lg xl:max-h-lg mt-8 rounded-full p-6 xl:p-10 border-2 border-slate-400 hover:ring-2 transition-all duration-500 object-cover"
+            src={nasmiPic}
+            alt="Nasmi's Picture"
+          />
         </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
